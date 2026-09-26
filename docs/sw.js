4ae6f8de7fba7251fc27205a62420ae72c1db250
jorgeslope73-debug <jorgeslope73@gmail.com>
2026-09-26 19:23:40 +0200

    V19.11: actualizar cache del service worker

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'V19.10';
+const VERSION = 'V19.11';
 const SHELL_CACHE = `galaxy-combat-shell-${VERSION}`;
 const ASSET_CACHE = `galaxy-combat-assets-${VERSION}`;
 const ACTIVE_CACHES = new Set([SHELL_CACHE, ASSET_CACHE]);
